9aae96f89fd84280deddf5296d018de6f0737ae8
OmGade <uday1982c@gmail.com>
2016-06-02 22:53:57 -0500

    Sample Git Project

---
+++ GitProject/src/GitjavaClass.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author OmsriudayaKumar Gade
+ *
+ */
+public class GitjavaClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+
+	}
+
+}
